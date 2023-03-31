ebd215c6df1b8163ce5de06ae7c9313be3868782
BlackCode777 <blackcode77.1980@gmail.com>
2023-03-31 15:33:25 -0300

    Inicio do projeto - incluindo arquivos não rastreados

---
+++ RotinaAdminSalaoReinoBackend/src/main/java/com/rotinaadminsalaoreinobackend/RotinaAdminSalaoReinoBackend/RotinaAdminSalaoReinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rotinaadminsalaoreinobackend.RotinaAdminSalaoReinoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotinaAdminSalaoReinoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotinaAdminSalaoReinoBackendApplication.class, args);
+	}
+
+}
---
+++ RotinaAdminSalaoReinoBackend/src/test/java/com/rotinaadminsalaoreinobackend/RotinaAdminSalaoReinoBackend/RotinaAdminSalaoReinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotinaadminsalaoreinobackend.RotinaAdminSalaoReinoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotinaAdminSalaoReinoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
