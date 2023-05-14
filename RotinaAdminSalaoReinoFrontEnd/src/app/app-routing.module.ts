6cb3b21d25ff63c15eb363ede9ac3a5e47ff2073
BlackCode777 <blackcode77.1980@gmail.com>
2023-05-14 12:51:07 -0300

    ajuste de rotas, inclusão de campos input para tela de login e ajustres de centralização de títulos e botões.

--- RotinaAdminSalaoReinoFrontEnd/src/app/app-routing.module.ts
+++ RotinaAdminSalaoReinoFrontEnd/src/app/app-routing.module.ts
@@ -9,13 +9,10 @@ const routes: Routes = [
         {  path: 'home', component: HomeComponent  }
 
         /*
-        { path: '', redirectTo: '/login', pathMatch: 'full' },
-        { path: '', loadChildren: () => 
-                import('./login/login.module').then(m => m.LoginModule) },      
-        { path: 'home', loadChildren: () => 
-                import('./home/home.module').then(m => m.HomeModule) },
-        */
-               
+        { path: '', redirectTo: '/login', pathMatch: 'full' }, 
+        { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },      
+        { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+        */               
 
 ];
 
